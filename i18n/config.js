3e75e02c8400fe822f4b29a0d71be6eac4508f31
Bagus <tribagus0510@gmail.com>
2025-02-21 19:56:46 +0700

    fix

--- i18n/config.js
+++ i18n/config.js
@@ -2,10 +2,17 @@ export const locales = ["id", "en"];
 
 export const pathnames = {
     "/": "/",
-    "/about-us": "/about-us",
+    "/about": "/about",
+    "/about/locations": "/about/locations",
+    "/about/team": "/about/team",
     "/contact-us": "/contact-us",
-    "/training": "/training",
+    "/services": "/services",
+    "/services/digital-marketing": "/services/digital-marketing",
+    "/services/maintenance-services": "/services/maintenance-services",
+    "/services/web-development": "/services/web-development",
+    "/partners": "/partners",
     "/privacy-policy": "/privacy-policy",
+    "/terms-of-service": "/terms-of-service",
 };
 
 export const getLocales = () => locales;
